99b6ae221ad8d1d39ef4f3bbf9729b6329a4f107
etrex kuo <et284vu065k3@gmail.com>
2018-04-22 10:15:52 +0800

    3.3 從 Postman 認識 HTTP 協定

--- ironman/app/controllers/kamigo_controller.rb
+++ ironman/app/controllers/kamigo_controller.rb
@@ -1,4 +1,10 @@
 class KamigoController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def webhook
+    render plain: params
+  end
+
   def eat
     render plain: "吃土啦"
   end
