e8034d408b9e89cf6fc06968057244dbdfd210e3
etrex kuo <et284vu065k3@gmail.com>
2018-04-22 11:17:10 +0800

    4.4 卡米狗學說話

--- ironman/app/controllers/kamigo_controller.rb
+++ ironman/app/controllers/kamigo_controller.rb
@@ -3,8 +3,11 @@ class KamigoController < ApplicationController
   protect_from_forgery with: :null_session
 
   def webhook
+    # 學說話
+    reply_text = learn(received_text)
+
     # 設定回覆文字
-    reply_text = keyword_reply(received_text)
+    reply_text = keyword_reply(received_text) if reply_text.nil?
 
     # 傳送訊息到line
     response = reply_to_line(reply_text)
@@ -19,15 +22,27 @@ class KamigoController < ApplicationController
     message['text'] unless message.nil?
   end
 
+  # 學說話
+  def learn(received_text)
+    #如果開頭不是 卡米狗學說話; 就跳出
+    return nil unless received_text[0..6] == '卡米狗學說話;'
+
+    received_text = received_text[7..-1]
+    semicolon_index = received_text.index(';')
+
+    # 找不到分號就跳出
+    return nil if semicolon_index.nil?
+
+    keyword = received_text[0..semicolon_index-1]
+    message = received_text[semicolon_index+1..-1]
+
+    KeywordMapping.create(keyword: keyword, message: message)
+    '好哦∼好哦∼'
+  end
+
   # 關鍵字回覆
   def keyword_reply(received_text)
-    # 學習紀錄表
-    keyword_mapping = {
-      'QQ' => '神曲支援:https://www.youtube.com/watch?v=T0LfHEwEXXw&feature=youtu.be&t=1m13s',
-      '我難過' => '神曲支援:https://www.youtube.com/watch?v=T0LfHEwEXXw&feature=youtu.be&t=1m13s'
-    }
-    # 查表
-    keyword_mapping[received_text]
+    KeywordMapping.where(keyword: received_text).last&.message
   end
 
   # 傳送訊息到line
