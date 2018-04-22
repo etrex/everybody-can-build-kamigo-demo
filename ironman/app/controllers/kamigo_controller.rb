dd753555c7d1b447ee5f6d29ba8c937d63e4cdc6
etrex kuo <et284vu065k3@gmail.com>
2018-04-22 10:08:38 +0800

    3.2 從 Rails 認識 HTTP 協定

--- ironman/app/controllers/kamigo_controller.rb
+++ ironman/app/controllers/kamigo_controller.rb
@@ -2,4 +2,29 @@ class KamigoController < ApplicationController
   def eat
     render plain: "吃土啦"
   end
+
+  def request_headers
+    render plain: request.headers.to_h.reject{ |key, value|
+      key.include? '.'
+    }.map{ |key, value|
+      "#{key}: #{value}"
+    }.sort.join("\n")
+  end
+
+  def request_body
+    render plain: request.body
+  end
+
+  def response_headers
+    response.headers['5566'] = 'QQ'
+    render plain: response.headers.to_h.map{ |key, value|
+      "#{key}: #{value}"
+    }.sort.join("\n")
+  end
+
+  def show_response_body
+    puts "===這是設定前的response.body:#{response.body}==="
+    render plain: "虎哇花哈哈哈"
+    puts "===這是設定後的response.body:#{response.body}==="
+  end
 end
--- ironman/config/routes.rb
+++ ironman/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get '/kamigo/eat', to: 'kamigo#eat'
+  get '/kamigo/request_headers', to: 'kamigo#request_headers'
+  get '/kamigo/request_body', to: 'kamigo#request_body'
+  get '/kamigo/response_headers', to: 'kamigo#response_headers'
+  get '/kamigo/response_body', to: 'kamigo#show_response_body'
 end
