da16dc391ec21b8a9b54012420a2713f43680061
etrex kuo <et284vu065k3@gmail.com>
2018-04-22 10:49:25 +0800

    4.2 串接 Line Messaging API Webhook

--- ironman/app/controllers/kamigo_controller.rb
+++ ironman/app/controllers/kamigo_controller.rb
@@ -1,8 +1,28 @@
+require 'line/bot'
 class KamigoController < ApplicationController
   protect_from_forgery with: :null_session
 
   def webhook
-    render plain: params
+    # Line Bot API物件初始化
+    client = Line::Bot::Client.new { |config|
+      config.channel_secret = '9160ce4f0be51cc72c3c8a14119f567a'
+      config.channel_token = '2ncMtCFECjdTVmopb/QSD1PhqM6ECR4xEqC9uwIzELIsQb+ I4wa/s3pZ4BH8hCWeqfkpVGVig/mIPDsMjVcyVbN/WNeTTw5eHEA7hFhaxPmQSY2Cud51LKPPi XY+nUi+QrXy0d7Hi2YUs65B/tVOpgdB04t89/1O/w1cDnyilFU='
+    }
+
+    # 取得reply token
+    reply_token = params['events'][0]['replyToken']
+
+    # 設定回覆訊息
+    message = {
+      type: 'text',
+      text: '好哦∼好哦∼'
+    }
+
+    # 傳送訊息
+    response = client.reply_message(reply_token, message)
+
+    # 回應200
+    head :ok
   end
 
   def eat
